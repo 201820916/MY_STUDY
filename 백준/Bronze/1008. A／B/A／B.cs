fdcaca2b774fa26c7f402bc12cb25e5d6c68d35d
Park Seong Hun <kkupsh2@gmail.com>
2025-11-10 19:08:05 +0900

    [Bronze V] Title: A/B, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace BAEKJOON
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            string[] parts = input.Split(' ');
+            int A = int.Parse(parts[0]);
+            int B = int.Parse(parts[1]);
+            double result = (double)A/B;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
