168e7e19a16ed6420094c8720940a8be06c3fd5d
Park Seong Hun <kkupsh2@gmail.com>
2025-11-15 18:02:43 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
