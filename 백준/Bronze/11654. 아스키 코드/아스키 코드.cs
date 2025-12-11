4c59d46eb0cbec1ad3dc2507b92848ba9286d29d
Park Seong Hun <kkupsh2@gmail.com>
2025-12-11 16:46:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace beakjoon
+{
+    class project
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            char c = input[0];
+            
+            Console.WriteLine((int)c);
+        }
+    }
+}
