fe6e552c3b6c8d28c559f39086a67d5e5b980678
Park Seong Hun <kkupsh2@gmail.com>
2025-12-11 16:24:37 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 48 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace beakjoon
+{
+    class project
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            int i = 0;
+            
+            Console.WriteLine(s.Length);
+         }
+    }
+}
