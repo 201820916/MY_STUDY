41f9b391e7e6ace59c26e65bdcbdaf97a8cd0e32
Park Seong Hun <kkupsh2@gmail.com>
2026-03-05 18:09:27 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 48 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace beakjoon
+{
+    class project
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                
+                if (input == null)
+                {
+                    break;
+                }
+                
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
